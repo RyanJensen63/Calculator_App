91ab39425476c03a3adf804d41ff41f8336b3d96
Ryan Jensen <ryanjensen63@gmail.com>
2022-04-22 14:48:32 -0500

    Re-did the Advanced Calculator to help make a menu later down the road to merge everything together

--- src/calculator/device/AdvanceCalc.java
+++ src/calculator/device/AdvanceCalc.java
@@ -1,15 +1,48 @@
-package calculator.device;
-import java.util.Scanner;
-import java.lang.Math;
-public class AdvanceCalc {
+public class advanceCalc implements advanceMath {
+    String problem="";
+    double square_root_value=0;
+    double base_number= 0;
+    double exponet_number=0;
+    String answer="";
+    advanceCalc(String problem){
+        this.problem = problem;
+    }
 
-		
-	}
+    public String squareRoot(){
+        String result ="";
+        result = ""+Math.sqrt(square_root_value);        
+        return result;
+    }
+    public String powerOf(){
+        String result ="";
+        result = ""+Math.pow(base_number, exponet_number);
+        return result;
+    }
+    
+    public String solve(){
+        try{
+            
+            if(this.problem.contains("S")){
+                String soln = this.problem.replace("S", "").trim();
+                this.square_root_value = Double.parseDouble(soln);
+                answer =squareRoot();
 
+                
+            }else if(this.problem.contains("^")){
+                String temp_box[] = this.problem.split("\\^");
+                this.base_number = Double.parseDouble(temp_box[0]);
+                this.exponet_number = Double.parseDouble(temp_box[1]);
+                answer = powerOf();
+               
 
-
-
-
-
-
-
+                
+            }else{
+                answer ="Please enter the math correctly";
+            }
+        }catch(Exception x){
+            System.out.println("Please enter the math properly");
+        }
+    return answer;
+    }
+     
+}
